d5fd58632192cf1cabfde5ee3e2530ab6b1abf11
Praveenmaila <praveenmaila249@gmail.com>
2025-09-22 11:08:56 +0530

    function in js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,27 @@
-var a =22;
-var a =33;
-console.log(a);
 
+obj1 ={
+    name : "pranav",
+    age : 22,
+    city : "Hyderabad"
+};
+obj2 ={
+    name : "praveen",
+    age : 23,
+    city : "bangalore"
+};
+obj3 ={
+    name : "anirudh",
+    age : 22,
+    city : "chennai"
+};
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+function printDetails(obj) {
+    console.log("Name: " + obj.name);
+    console.log("Age: " + obj.age);
+    console.log("City: " + obj.city);
+}
+printDetails(obj1);
+printDetails(obj2);
+printDetails(obj3);
