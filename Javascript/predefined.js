503e968af9d98bbefe97017c6d025647ec679692
Praveenmaila <praveenmaila249@gmail.com>
2025-09-22 16:21:34 +0530

    mofified the article and added predefined

---
+++ Javascript/predefined.js
@@ -0,0 +1,15 @@
+const a=Number(prompt("Enter a number:"));
+const b=Number(prompt("Enter another number:")) ;
+document.writeln("you entered a= ",a," b= ",b);
+ function add(){
+    alert(a+b);
+ }
+    function sub(){
+    alert("The difference is: "+(a-b));
+ }
+    function mul(){
+    alert("The product is: "+(a*b));
+ }
+    function div(){
+    alert("The quotient is: "+(a/b));
+ }
