a62fe325998de138959eaf2457388bd592054487
Praveenmaila <164868094+Praveenmaila@users.noreply.github.com>
2025-09-22 12:10:34 +0530

    Create strings.js

---
+++ Javascript/strings.js
@@ -0,0 +1,8 @@
+let s="Hello, World!"
+console.log(s);
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.slice(0,5));
+console.log(s.includes("World"));
+s.replace("World","Everyone");
